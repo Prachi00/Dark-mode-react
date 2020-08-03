95bfafbed46f329d78567a22d4b23eeab2def2c8
Prachi <darshana.prachi@jungleworks.com>
2020-08-03 13:14:43 +0530

    react dark theme

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-## Usage
+# DarkMode
 
-```
-git clone https://github.com/rossanodan/react-dark-theme.git
-cd react-dark-theme
-npm start
-```
+<!-- ![Giphy](https://media.giphy.com/media/26DNeo2xDmfj3plbW/giphy.gif) -->
+
+[Live Demo](http://Prachi00.github.io/dark-mode-react)
+
+The classic dark mode app with a toggle that uses localstorage.
+
+## Features
+
+* Preference color scheme
+* Local storage
+* Lightweight
+
+## Development
+
+Install all the required packages/dependencies using yarn or npm
+
+
+## Contributing
+
+Feel free to open issues and pull requests!
---
+++ build/precache-manifest.e5413426a413ba8878b45b40934f5a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb65803174643c7a78dd0055726e9533",
+    "url": "/dark-mode-react/index.html"
+  },
+  {
+    "revision": "979945ea39963799bfdf",
+    "url": "/dark-mode-react/static/css/main.f55e97f1.chunk.css"
+  },
+  {
+    "revision": "8a88f5abed93e269b4fc",
+    "url": "/dark-mode-react/static/js/2.377f1f4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dark-mode-react/static/js/2.377f1f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "979945ea39963799bfdf",
+    "url": "/dark-mode-react/static/js/main.3cfe2f03.chunk.js"
+  },
+  {
+    "revision": "bd332cfa130a66ff4003",
+    "url": "/dark-mode-react/static/js/runtime-main.81ae2151.js"
+  }
+]);
